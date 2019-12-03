aafdd3402b825e2b3d0f2a240c40f94d4aa039b8
Sam Singh <sqrtmango@gmail.com>
2019-12-03 07:48:17 -0500

    Fixed async AGAIN???

--- src/items/src/ItemRepo.ts
+++ src/items/src/ItemRepo.ts
@@ -67,7 +67,7 @@ export async function addItem(
     const createdItem = await Item.create(itemDoc);
 
     // Add item to the user.
-    Message.sendMessage("user_queue", "ADD_ITEM", {
+    await Message.sendMessage("user_queue", "ADD_ITEM", {
       username,
       itemid: createdItem._id
     });
@@ -91,7 +91,7 @@ export async function addItem(
       content: createdItem.content
     };
 
-    Message.sendMessage("search_queue", "INDEX_ITEM", { doc });
+    await Message.sendMessage("search_queue", "INDEX_ITEM", { doc });
 
     // Associate media items.
     const associateRequests: any[] = [];
@@ -125,7 +125,7 @@ export async function deleteItem(username: string, itemid: string) {
     // Item owned by user. Delete.
     if (item.user === username) {
       // Delete item from user.
-      Message.sendMessage("user_queue", "DELETE_ITEM", {
+      await Message.sendMessage("user_queue", "DELETE_ITEM", {
         username,
         itemid
       });
